ebd953452a8ce59c3df93e90a3bc5b15eaafd31e
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-13 15:13:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,9 +27,9 @@ def won?(board)
   
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combo
-  elsif
-    position_1 == "O" && position_2 == "O" && position_3 == "O"
+  elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
     return win_combo
+  end 
   
   
 end 

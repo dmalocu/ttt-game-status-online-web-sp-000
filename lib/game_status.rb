363c7545535894a4300d05933de71687a97220f6
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-13 15:46:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,8 +24,8 @@ def won?(board)
       return win_combination
     elsif (board[win_combination[0]]) == "O" && (board[win_combination[1]]) == "O" && (board[win_combination[2]]) == "O" 
       return win_combination
-    else
-      return false
+    end 
+    false 
     end
   end
 end

36f16c4f081cea842597650beb63e796755f093b
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-13 15:42:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,18 +20,9 @@ def won?(board)
     return false 
   end
   WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+    if board(win_combination[0]) == "X" && board(win_combination[1]) == "X" && board(win_combination[2]) == "X" 
+        return win_combination
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    else 
-      false 
     end
   end
 end

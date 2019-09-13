383d006334d524b1d77581cf7661c7fe18962353
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-13 20:15:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,8 +51,16 @@ def over?(board)
   end
 end 
 
-def winner(board)
-  if won?(board).all? == "X"
-    return "X"
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end

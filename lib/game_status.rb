31284747043676f7f4e224f323549d2e141ebe73
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-13 15:03:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,7 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  top_row_win = [0,1,2],
+  [0,1,2],
   [3,4,5],
   [6,7,8],
   [0,3,6],
@@ -14,3 +14,5 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
   ]
+  
+  
